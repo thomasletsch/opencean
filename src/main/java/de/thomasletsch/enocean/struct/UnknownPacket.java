05df640ef0d06e2555e907fa9b0f915a9177dda7
Evert <evertvanes@gmail.com>
2013-04-24 22:45:03 +0200

    added rps packet

--- src/main/java/de/thomasletsch/enocean/struct/UnknownPacket.java
+++ src/main/java/de/thomasletsch/enocean/struct/UnknownPacket.java
@@ -14,8 +14,10 @@ public class UnknownPacket extends BasicPacket {
 		case RadioPacket1BS.RADIO_TYPE:
 			return RadioPacket1BS.ResolvedPacket(this);
 
+		case RadioPacketRPS.RADIO_TYPE:
+			return RadioPacketRPS.ResolvedPacket(this);
+
 		case RadioPacket.RADIO_TYPE_4BS:
-		case RadioPacket.RADIO_TYPE_RPS:
 		case RadioPacket.RADIO_TYPE_VLD:
 			return RadioPacket.ResolvedPacket(this);
 
