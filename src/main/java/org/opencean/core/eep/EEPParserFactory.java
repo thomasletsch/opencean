7fbff45da0d5a8208fe01e95a5a0d379ccb349e9
Markus Rathgeb <maggu2810@gmail.com>
2014-06-23 09:44:11 +0200

    EEP A5:08:02 Reorganize to a more generic version

--- src/main/java/org/opencean/core/eep/EEPParserFactory.java
+++ src/main/java/org/opencean/core/eep/EEPParserFactory.java
@@ -13,7 +13,7 @@ public class EEPParserFactory {
         parser.put(EEPId.EEP_D2_01_08, new PowerSensor(EEPId.EEP_D2_01_08));
         parser.put(EEPId.EEP_A5_02_05, new TemperaturSensor(0, 40, EEPId.EEP_A5_02_05));
         parser.put(EEPId.EEP_A5_04_01, new TempHumiditySensor(0, 40, 0, 100, EEPId.EEP_A5_04_01));
-        parser.put(EEPId.EEP_A5_08_02, new LightTempertureAndOccupancySensor());
+        parser.put(EEPId.EEP_A5_08_02, new LightTempertureAndOccupancySensor(EEPId.EEP_A5_08_02));
         parser.put(EEPId.EEP_A5_07_03, new OccupancySensor());
         parser.put(EEPId.EEP_F6_02_01, new RockerSwitch());
         parser.put(EEPId.EEP_F6_02_02, new RockerSwitch());
--- src/main/java/org/opencean/core/packets/data/PacketDataEEPA50802.java
+++ src/main/java/org/opencean/core/packets/data/PacketDataEEPA50802.java
@@ -5,63 +5,20 @@
  */
 package org.opencean.core.packets.data;
 
-import org.opencean.core.common.values.Unit;
-import org.opencean.core.eep.CalculationUtil;
-
 /**
  *
  * @author rathgeb
  */
-public class PacketDataEEPA50802 extends PacketData4BS {
+public class PacketDataEEPA50802 extends PacketDataEEPA508 {
 
-    public static final int SUPPLY_VOLTAGE_RANGE_MIN = 0;
-    public static final int SUPPLY_VOLTAGE_RANGE_MAX = 255;
-    public static final double SUPPLY_VOLTAGE_SCALE_MIN = 0;
-    public static final double SUPPLY_VOLTAGE_SCALE_MAX = 5.1;
-    public static final Unit SUPPLY_VOLTAGE_UNIT = Unit.VOLTAGE;
-
-    public static final int ILLUMINATION_RANGE_MIN = 0;
-    public static final int ILLUMINATION_RANGE_MAX = 255;
     public static final double ILLUMINATION_SCALE_MIN = 0;
-    public static final double ILLUMINATION_SCALE_MAX = 510;
-    public static final Unit ILLUMINATION_UNIT = Unit.LUX;
+    public static final double ILLUMINATION_SCALE_MAX = 1020;
 
-    public static final int TEMPERATURE_RANGE_MIN = 0;
-    public static final int TEMPERATURE_RANGE_MAX = 255;
     public static final double TEMPERATURE_SCALE_MIN = 0;
     public static final double TEMPERATURE_SCALE_MAX = 51;
-    public static final Unit TEMPERATURE_UNIT = Unit.DEGREE_CELSIUS;
-
-    public PacketDataEEPA50802() {
-    }
 
     public PacketDataEEPA50802(byte[] eepData) {
-        super(eepData);
+        super(eepData, ILLUMINATION_SCALE_MIN, ILLUMINATION_SCALE_MAX, TEMPERATURE_SCALE_MIN, TEMPERATURE_SCALE_MAX);
     }
 
-    public double getSupplyVoltage() {
-        final long range = getDataRange(3, 7, 3, 0);
-        final double scale = CalculationUtil.rangeToScale(range, SUPPLY_VOLTAGE_RANGE_MIN, SUPPLY_VOLTAGE_RANGE_MAX, SUPPLY_VOLTAGE_SCALE_MIN, SUPPLY_VOLTAGE_SCALE_MAX);
-        return scale;
-    }
-
-    public double getIllumination() {
-        final long range = getDataRange(2, 7, 2, 0);
-        final double scale = CalculationUtil.rangeToScale(range, ILLUMINATION_RANGE_MIN, ILLUMINATION_RANGE_MAX, ILLUMINATION_SCALE_MIN, ILLUMINATION_SCALE_MAX);
-        return scale;
-    }
-
-    public double getTemperature() {
-        final long range = getDataRange(1, 7, 1, 0);
-        final double scale = CalculationUtil.rangeToScale(range, TEMPERATURE_RANGE_MIN, TEMPERATURE_RANGE_MAX, TEMPERATURE_SCALE_MIN, TEMPERATURE_SCALE_MAX);
-        return scale;
-    }
-
-    public boolean isPIRStatusOn() {
-        return getDataBit(0, 1) == 0;
-    }
-
-    public boolean isOccupancyButtonPressed() {
-        return getDataBit(0, 0) == 0;
-    }
 }
